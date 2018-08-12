d004af0897a90fad35949b6de3fec612a851ff1a
jagapathibabumekala <41908920+jagapathibabumekala@users.noreply.github.com>
2018-08-12 07:21:43 -0700

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+       int n,sum=0;
+       Scanner s=new Scanner(System.in);
+         n=s.nextInt();
+         sum=n*(n+1)/2;
+         System.out.println(sum);
+	}
+}
