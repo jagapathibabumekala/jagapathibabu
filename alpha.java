e3c8fd6d7e751c1ad9a9d236824f8d4caf9bad10
jagapathibabumekala <41908920+jagapathibabumekala@users.noreply.github.com>
2018-08-12 06:52:12 -0700

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch;
+		Scanner s=new Scanner(System.in);
+		 ch=s.next().charAt(0);
+		 if((ch>='a'&&ch<= 'z')||(ch>='A'&&ch<='Z'))
+		 {
+		 	System.out.println("Alphabet");
+		 }
+		 else
+		 {
+		 	System.out.println("No");
+		 }
+	}
+}
