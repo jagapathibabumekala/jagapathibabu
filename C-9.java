e7894b9dd438ae3cf730633f9f0ce4e94f474f0e
jagapathibabumekala <41908920+jagapathibabumekala@users.noreply.github.com>
2018-08-12 23:13:37 -0700

    Create C-9.java

---
+++ C-9.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int hr,mi;
+		hr=n/60;
+		mi=n%60;
+		System.out.println(hr+" "+mi);
+	}
+}
