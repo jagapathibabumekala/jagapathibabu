31aa6c23843f2443eb7de812cc3ef2075a099147
jagapathibabumekala <41908920+jagapathibabumekala@users.noreply.github.com>
2018-08-22 22:41:49 -0700

    Create d-3.java

---
+++ d-3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s1=new Scanner(System.in);
+		String s=s1.nextLine();
+		int count=0;
+		for(char c:s.toCharArray())
+		{
+			if(c==' ')
+			count++;
+		}
+		System.out.println(count);
+	}
+}
