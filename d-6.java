48e425f1366af6076b934130bd1427549bf2e397
jagapathibabumekala <41908920+jagapathibabumekala@users.noreply.github.com>
2018-08-22 22:50:39 -0700

    Create d-6.java

---
+++ d-6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   	Scanner s1=new Scanner(System.in);
+		String s=s1.nextLine();
+		int count=0;
+		for(char c:s.toCharArray())
+		{
+			if(Character.isDigit(c));
+			else if(Character.isLetter(c));
+			else if(c==' '||c=='.');
+			else
+			count++;
+		}
+		System.out.println(count);
+	}
+}
