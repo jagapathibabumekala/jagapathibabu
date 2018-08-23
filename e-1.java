4973f7f4807378b579fe14d8799c31de8b536ea9
jagapathibabumekala <41908920+jagapathibabumekala@users.noreply.github.com>
2018-08-22 23:48:35 -0700

    Create e-1.java

---
+++ e-1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s1=new Scanner(System.in);
+		String s=s1.next();
+		int n=s1.nextInt();
+		for(int i=0;i<n;i++)
+	           System.out.println(s);
+	}
+}
