c197c28a030c47be8dd596806316d3586f219ba5
jagapathibabumekala <41908920+jagapathibabumekala@users.noreply.github.com>
2018-08-24 00:23:29 -0700

    Create f-6.java

---
+++ f-6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String s=s.next();
+		int count=0;
+		for(int i=0;i<s.length();i++)
+		{
+			char ch=s.charAt(i);
+			if(Character.isDigit(ch)||Character.isLetter(ch))
+			count++;
+		}
+		if(count==0)
+		System.out.println("No");
+		else
+		   System.out.println("Yes");
+	}
+}
