c39436135a0ecbe9c0f992fd0a1b186d507e6cb8
jagapathibabumekala <41908920+jagapathibabumekala@users.noreply.github.com>
2018-08-24 00:01:02 -0700

    Create e-9.java

---
+++ e-9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int m=s.nextInt();
+          int avg=m+n;
+          System.out.println(avg);
+	}
+}
